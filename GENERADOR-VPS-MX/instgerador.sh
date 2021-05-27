83dd29022522024450d3aa7c11af963bef4dca7b
diesel09 <37728951+diesel09@users.noreply.github.com>
2021-05-27 02:42:17 -0500

    Update instgerador.sh

--- GENERADOR-VPS-MX/instgerador.sh
+++ GENERADOR-VPS-MX/instgerador.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 IVAR="/etc/http-instas"
 SCPT_DIR="/etc/SCRIPT"
-SCPresq="aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL2RpZXNlbDA5L2dlbm14OC4zL21hc3Rlci9HRU5FUkFET1ItVlBTLU1YL2dlcmFkb3I="
+SCPresq="aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL2RpZXNlbDA5LzguNC9tYXN0ZXIvZ2VyYWRvcg=="
 SUB_DOM='base64 -d'
 rm $(pwd)/$0
 ofus () {
