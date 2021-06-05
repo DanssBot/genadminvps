582c2c7ce2f5e00a6115dfe09f3933bbd7dc8b43
diesel09 <37728951+diesel09@users.noreply.github.com>
2021-06-05 14:03:50 -0500

    Update v2ray.sh

--- GENERADOR-VPS-MX/gerador/v2ray.sh
+++ GENERADOR-VPS-MX/gerador/v2ray.sh
@@ -242,7 +242,7 @@ echo -ne "\033[1;32m [10] > " && msg -azu "ESTADISTICAS DE CONSUMO "
 echo -ne "\033[1;32m [11] > " && msg -azu "LIMPIADOR DE EXPIRADOS ------- $statgen\n$(msg -bar)"
 msg -bar && echo -ne "$(msg -verd "[0]") $(msg -verm2 ">") "&& msg -bra "\033[1;41mREGRESAR AL MENU"
 msg -bar
-while [[ ${arquivoonlineadm} != @(0|[1-9]) ]]; do
+while [[ ${arquivoonlineadm} != @(0|[1-11]) ]]; do
 read -p "[0-11]: " arquivoonlineadm
 tput cuu1 && tput dl1
 done
