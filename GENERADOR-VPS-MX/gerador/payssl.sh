c16e24775c444916e1f94847ccf8a6a83c24b046
diesel09 <37728951+diesel09@users.noreply.github.com>
2021-09-22 13:06:43 -0500

    Update payssl.sh

--- GENERADOR-VPS-MX/gerador/payssl.sh
+++ GENERADOR-VPS-MX/gerador/payssl.sh
@@ -326,6 +326,6 @@ iptables -I INPUT -p tcp --dport 443 -j ACCEPT
 
 echo -e "ps x | grep 'pythonwe' | grep -v 'grep' || screen -dmS pythonwe python proxy.py -p 80" >> /etc/autostart
 
-echo -e "\033[1;32m            INSTALACION CONCLUIDA "
+echo -e "\033[1;32m            INSTALACION COMPLETADA "
 
 echo 
