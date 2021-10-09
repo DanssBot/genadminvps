24a6a3813cedb3e8d1f312f61fae4321895a1300
DanssBot <90471487+DanssBot@users.noreply.github.com>
2021-10-09 02:23:57 -0500

    Update instgerador.sh

--- GENERADOR-VPS-MX/instgerador.sh
+++ GENERADOR-VPS-MX/instgerador.sh
@@ -88,13 +88,11 @@ sed -i "s;Listen 80;Listen 81;g" /etc/apache2/ports.conf
 service apache2 restart > /dev/null 2>&1 &
 IVAR2="/etc/key-gerador"
 echo "$Key" > $IVAR2
-cp /bin/http-server.sh /etc/SCRIPT
-mv /etc/SCRIPT/http-server.sh /etc/SCRIPT/http-server.py
-chmod +x /etc/SCRIPT/http-server.py
-wget https://raw.githubusercontent.com/DanssBot/genadminvps/master/GENERADOR-VPS-MX/gerador/gerar.sh &>/dev/null
-mv gerar.sh /etc/SCRIPT
-chmod +x /etc/SCRIPT/gerar.sh
-cd /etc/SCRIPT/
+cp $HOME/lista-arq /etc/gerador
+cp /bin/http-server.sh /etc/gerador
+mv /etc/gerador/http-server.sh /etc/gerador/http-server.py
+mv gerar.sh /etc/gerador
+cd /etc/gerador
 rm -rf FERRAMENTA KEY KEY! INVALIDA!
 rm $HOME/lista-arq
 sed -i -e 's/\r$//' /usr/bin/gerar.sh
